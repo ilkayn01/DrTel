be62c7a22bdc01da52513ae2c7b77f2dc4dd5456
Ilanky <49631121+ilankyn@users.noreply.github.com>
2021-12-24 11:51:45 +0530

    compress, plot, angle

---
+++ merge.py
@@ -0,0 +1,28 @@
+def drive(loc_files):
+  frame = []
+  for i in loc_files:
+    d = pd.read_csv(i)
+    frame.append(d)
+  return frame
+
+def plot(file):
+  frame = drive(file)
+  fig = plt.figure(figsize = (12, 7))
+  for i in frame:
+    plt.plot(i['x'], i['y'])
+  plt.show()
+
+#determines sharp turns based on parameters
+def binary(x, limit, ref):
+  for i in ref:
+    if (i >= limit):
+      x.append(1)
+    else:
+      x.append(0)
+  return x
+
+def angle_to(x2, y2, x1, y1, rotation=0, clockwise=False):
+    angle = degrees(atan2(y2-y1, x2-x1)) - rotation
+    if not clockwise:
+        angle = -angle
+    return angle % 360
